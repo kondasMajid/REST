3edeac97a7d145bdb0219bde12ad3090c6ce9923
kondasMajid <kondasmajid@gmail.com>
2020-01-29 04:15:52 -0800

    changes

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,14 +7,8 @@ import { HttpClient,HttpHeaders , HttpErrorResponse } from '@angular/common/http
 
 export class DataService {
   countryApi = 'xxxxxxxxxxxxxxxxxxxxxxxxxx';
-
   data;
-
   apis = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
-
-  // token = 'klloyds-user-token';
-
-  // value = 'B2A8AA92FE13A3FFD83FEF159A47A7E8';
 
   constructor(private http: HttpClient) {  }
 
@@ -22,7 +16,6 @@ export class DataService {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
     'xxxxxxxxxxxxx-user-token': 'xxxxxxxxxxxxx'
-
    }
     requestOptions = {                                                                                                                                                                                 
     headers: new HttpHeaders(this.headerDict) 
